921404faefa8e7f630b6deef92963f85dc64aff3
Oleg Patsey <o.patcez@sigma-it.ru>
2021-12-07 23:01:31 +0300

    docker compose servers: postgresDB

--- server/db-backup/script.sh
+++
@@ -1,7 +0,0 @@
-#Export
-pg_dump -s -h localhost -d db-store -U postgres  -f myStoreSchema.sql
-pg_dump -a --inserts -h localhost -d db-store -U postgres  -f myStoreData.sql
-
-#create  db study. and  import dump
-psql -h localhost -d study -U postgres -f myStoreSchema.sql
-psql -h localhost -d study -U postgres -f myStoreData.sql
---
+++ server/db/init/init.sql
@@ -0,0 +1,13 @@
+CREATE SCHEMA "db-store";
+--
+--
+ALTER SCHEMA "db-store" OWNER TO postgres;
+--
+-- --
+-- -- Name: postgres; Type: SCHEMA; Schema: -; Owner: postgres
+-- --
+--
+CREATE SCHEMA postgres;
+--
+--
+ALTER SCHEMA postgres OWNER TO postgres;
